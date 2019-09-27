9f691801c2ca21fc503ee0d1c27e4dee0795257f
samuelmo <samuelmo78@gmail.com>
2019-09-26 18:54:11 -0600

    Updates

---
+++ precache-manifest.490513e2bbb54ebeaef909cff8cd79f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a167840436e9b7f8c3cdc734e37f18",
+    "url": "/Movie_App/index.html"
+  },
+  {
+    "revision": "233152f96a31f6f2efb7",
+    "url": "/Movie_App/static/css/main.05d37451.chunk.css"
+  },
+  {
+    "revision": "e5361ac94d838b5ca865",
+    "url": "/Movie_App/static/js/2.ff66c38a.chunk.js"
+  },
+  {
+    "revision": "233152f96a31f6f2efb7",
+    "url": "/Movie_App/static/js/main.094dbc08.chunk.js"
+  },
+  {
+    "revision": "b3cf72c3ad2eb218e6dd",
+    "url": "/Movie_App/static/js/runtime-main.9a5945ff.js"
+  }
+]);
